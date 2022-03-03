b3a35b5f2d853a5e73050ba4239eb255fb68090d
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-03 06:42:06 +0530

    added Vowal or Consonant Program

---
+++ Vowel_Or_Consonant/Vowel_Or_Consonant/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vowel_Or_Consonant
+{
+    public class VowelOrConsonant
+    {
+        public void Vowel_OR_Consonant(char letter)
+        {
+            if (letter == 'a' || letter == 'A' || letter == 'e' || letter == 'E' || letter == 'i' || letter == 'I' || letter == 'o' || letter == 'O' || letter == 'u' || letter == 'U')
+            {
+                Console.WriteLine($"Given letter : {letter} is Vowel");
+            }
+            else
+            {
+                Console.WriteLine($"Given letter : {letter} is Consonant");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Char Value");
+            char letter = Convert.ToChar(Console.ReadLine());
+            VowelOrConsonant voc = new VowelOrConsonant();
+            voc.Vowel_OR_Consonant(letter);
+        }
+    }
+}
