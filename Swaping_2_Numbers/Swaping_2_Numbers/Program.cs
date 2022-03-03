b7fbf799092f1acb7a60be0b0476ffd61d99de70
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-03 06:40:04 +0530

    added Swap Two Numbers Program

---
+++ Swaping_2_Numbers/Swaping_2_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Swaping_2_Numbers
+{
+    public class SwapNumber
+    {
+        int temp = 0;
+        public void Swapping(int number1, int number2)
+        {
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine($"After Swapping number 1 : {number1} number 2 : {number2}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number1 and number2");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Before Swapping number1 : {number1} number 2 : {number2}");
+            SwapNumber swap = new SwapNumber();
+            swap.Swapping(number1, number2);
+        }
+    }
+}
